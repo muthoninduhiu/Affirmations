34b6f9a70a1d329327f02f7b8b218e22fb1cfaab
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-04 11:36:40 +0300

    extend recycler view adapter

--- app/src/main/java/com/soni/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/soni/affirmations/adapter/ItemAdapter.kt
@@ -2,12 +2,15 @@ package com.soni.affirmations.adapter
 
 import android.content.Context
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.soni.affirmations.R
 import com.soni.affirmations.model.AffirmationsModel
 
-class ItemAdapter(val context: Context, private val dataset: List<AffirmationsModel>) {
+class ItemAdapter(val context: Context, private val dataset: List<AffirmationsModel>)
+    :RecyclerView.Adapter<ItemAdapter.ItemViewHolder{
     //creating a nested class
     class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view){
-
+        val textView: TextView = view.findViewById(R.id.item_title)
     }
 }
