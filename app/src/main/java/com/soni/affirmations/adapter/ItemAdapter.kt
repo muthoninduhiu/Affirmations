136de8dcde7a1ba423a2db328ba4b147768e5b87
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-04 11:39:21 +0300

    implement base class members

--- app/src/main/java/com/soni/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/soni/affirmations/adapter/ItemAdapter.kt
@@ -2,15 +2,28 @@ package com.soni.affirmations.adapter
 
 import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.soni.affirmations.R
 import com.soni.affirmations.model.AffirmationsModel
 
 class ItemAdapter(val context: Context, private val dataset: List<AffirmationsModel>)
-    :RecyclerView.Adapter<ItemAdapter.ItemViewHolder{
+    :RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
     //creating a nested class
     class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view){
         val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 }
