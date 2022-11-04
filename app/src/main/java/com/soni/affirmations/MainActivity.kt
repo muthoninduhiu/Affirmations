0b94ac38faf69a15af9413995eaf141a120b6a4d
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-04 11:14:07 +0300

    delete data from datasouce

--- app/src/main/java/com/soni/affirmations/MainActivity.kt
+++ app/src/main/java/com/soni/affirmations/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textview: TextView = findViewById(R.id.textview)
-        textview.text = DataSource().loadAffirmations().size.toString()
+
     }
 }
