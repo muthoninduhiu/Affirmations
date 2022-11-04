278f05cd6d1c362d44a456df4e9aa55e608d8cf3
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-11-04 11:52:49 +0300

    use the recycler view

--- app/src/main/java/com/soni/affirmations/MainActivity.kt
+++ app/src/main/java/com/soni/affirmations/MainActivity.kt
@@ -3,12 +3,20 @@ package com.soni.affirmations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.soni.affirmations.adapter.ItemAdapter
 import com.soni.affirmations.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //use recycler view
+        val myDataset = DataSource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        //improve perfomance
+        recyclerView.setHasFixedSize(true)
 
     }
 }
